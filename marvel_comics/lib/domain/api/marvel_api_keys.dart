1f0eb14cb23b8ac0012d4f14717b306bacf2da22
Anna (Domashych) Leushchenko <anna.domashych@gmail.com>
2022-05-15 19:06:22 +0300

    Declare Marvel API keys

--- marvel_comics/lib/domain/api/marvel_api_keys.dart
+++
@@ -1,7 +0,0 @@
-abstract class MarvelApiKeys {
-  // TODO: put your public key to defaultValue
-  static const publicKey = String.fromEnvironment('PUBLIC_API_KEY', defaultValue: '');
-
-  // TODO: put your private key to defaultValue
-  static const privateKey = String.fromEnvironment('PRIVATE_API_KEY', defaultValue: '');
-}
